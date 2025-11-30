8540cdd9772134c35fdfdcf50f98e68502474a72
MikeG404 <mk404@example.com>
2025-11-30 13:35:20 +0100

    Make dynamic links between NavFeatureSection and FeatureSection

--- src/components/sections/FeatureSection.tsx
+++ src/components/sections/FeatureSection.tsx
@@ -1,8 +1,9 @@
-function FeatureSection() {
+function FeatureSection({ renderView }) {
     return (
         <section className="h-full grow flex justify-center items-center p-8 border border-black rounded-2xl">
             <div>
                 <h1 className="text-5xl">DATATABLE OF ARTICLES</h1>
+                {renderView()}
             </div>
         </section>
     );
--- src/components/sections/NavFeatureSection.tsx
+++ src/components/sections/NavFeatureSection.tsx
@@ -1,13 +1,11 @@
-function NavFeatureSection() {
+function NavFeatureSection({ setView }) {
     return (
         <section className="max-w-xs h-full flex-1 flex flex-col justify-center p-8 border border-black rounded-2xl">
             <div>
-                {/* <ul>
-                    <li>Articles</li>
-                    <li>Articles</li>
-                    <li>Articles</li>
-                    <li>Articles</li>
-                </ul> */}
+                <ul>
+                    <li onClick={() => setView('articles')}>Articles</li>
+                    <li onClick={() => setView('dynamic-test')}>Dynamic Test</li>
+                </ul>
                 <h1 className="text-5xl">FEATURES</h1>
             </div>
         </section>
